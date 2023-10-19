162ba8e060d9c4d3eef18df134f08a363d327704
Daniel Fernando Pérez Sarmiento <daniel.perez@cpavs.mx>
2023-10-18 19:14:19 -0600

    configuración del proyecto y desarrollo del primer endpoint para guardar un usuario

---
+++ src/controllers/usuarios.controller.js
@@ -0,0 +1,26 @@
+const usuarioModel = require('../models/usuario.model')
+
+const create = async (req, res) => {
+    try {
+        let usuario = new usuarioModel({
+            nombre: req.body.nombre,
+            email: req.body.email,
+            password: req.body.password
+        });
+    
+        await usuario.save();
+    
+        return res.status(201).json({
+            message: "usuario creado exitosamente!"
+        });
+    } catch (error) {
+        return res.status(error.code).json({
+            message: "falló al crear el usuario!",
+            error: error.message
+        });
+    }
+};
+
+module.exports = {
+    create
+}
